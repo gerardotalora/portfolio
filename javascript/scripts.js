c6df0151bc7322e8e9225207e88f4470f58efbc6
gerard <otaloracanovas.g@husky.neu.edu>
2020-06-08 19:05:52 -0400

    About page redesign with collapsibles

---
+++ javascript/scripts.js
@@ -0,0 +1,11 @@
+function openEducation() {
+  document.getElementById("edu").classList.toggle("show");
+}
+
+function openExperience() {
+  document.getElementById("exp").classList.toggle("show");
+}
+
+function openSkills() {
+  document.getElementById("skl").classList.toggle("show");
+}
