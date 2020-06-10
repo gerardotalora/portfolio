0cc315c19da86b022f2794e480d5de5a68890edd
gerard <otaloracanovas.g@husky.neu.edu>
2020-06-10 17:11:25 -0400

    Final output

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,11 +1,23 @@
 function openEducation() {
   document.getElementById("edu").classList.toggle("show");
+  window.scrollBy({
+    top: 500,
+    behavior: 'smooth'
+  });
 }
 
 function openExperience() {
   document.getElementById("exp").classList.toggle("show");
+  window.scrollBy({
+    top: 500,
+    behavior: 'smooth'
+  });
 }
 
 function openSkills() {
   document.getElementById("skl").classList.toggle("show");
+  window.scrollBy({
+    top: 500,
+    behavior: 'smooth'
+  });
 }
